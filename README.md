a7f1a287392786111cbeb17a2469afe2659a56ee
Sunder Chakravarty <sunder.chak@swymcorp.com>
2022-02-11 19:34:14 +0530

    base version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # aranya
 smart forest related explorations
+
+## Sound Triangulation
+The objective is to detect the source of a sound source in a forest zone.
+
+3 or  pi's with sound detectors are placed in the forest zone
+The difference in the arrival time of the sound at these detectors along with their location is used to compute the location of the sound.
+
+The solution consists of
+
+1. Sound Detector Node
+	-  This is a pi with sound detector peripheral
+	-  This pi runs `shrona.py` which is a python script sends the SoundEvent to the Triangulator when a sound is heard.
+	-  SoundEvent has a timestamp and the identity of the pi
+2. Triangulator Node
+	-  This is a pi that listens to Sound Events raised by Sound Detector Node
+	-  This node knows the id and location of each of the Sound Detectors in the forest zone
+trikona	-  This pi runs `trikonate.py` which is a python script that computes the location of the sound source from the arrival time difference and the location of the Sound Detectors 
---
+++ shrona.py
@@ -0,0 +1,71 @@
+import RPi.GPIO as GPIO
+import socket
+from time import time, sleep
+import json
+import os
+
+# Create and configure logger
+import logging
+logging.basicConfig(filename="./shrona.log", format='%(asctime)s %(message)s', filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Utility class to keep track of elapsed time
+class TimeKeeper:
+    def __init__(self):
+        self.start_time = time()
+        self.last_time = self.start_time
+        self.elapsed_time = [0, 0]
+
+    def elapsed(self):
+        now = time()
+        self.elapsed_time[0] = now - self.last_time
+        self.elapsed_time[1] = now - self.start_time
+        self.last_time = now
+        return self
+
+    def __str__(self):
+        et = self.elapsed_time
+        return f"{et[0]:.2f}s, {et[1]:.2f}"
+
+    def test():
+        tk = TimeKeeper()
+        for i in range(10):
+            print(tk.elapsed())
+            sleep(.5)
+tk = TimeKeeper()
+
+# Main - SHRONA श्रोण hear
+SHRONA_DEVICE_ID = os.environ.get('SHRONA_DEVICE_ID', socket.gethostname())
+CHANNEL = int(os.environ.get('SHRONA_GPIO_CHANNEL', 17))
+UDP_PORT = int(os.environ.get('TRIKONATE_PORT', 5005))
+UDP_IP = os.environ.get(
+    'TRIKONATE_IP',
+    socket.gethostbyname_ex(socket.gethostname())[-1][-1]
+)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CHANNEL, GPIO.IN)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+
+def callback(channel):
+    tick = time()
+    global sock
+    g = GPIO.input(channel)
+    sound_event = {'device_id': SHRONA_DEVICE_ID, 'time': tick, 'level': g}
+    sound_json = json.dumps(sound_event)
+    sock.sendto(bytes(sound_json, 'ascii'), (UDP_IP, UDP_PORT))
+
+    global tk
+    tk.elapsed()
+    sound_event['elapsed'] = str(tk)
+    info =  json.dumps(sound_event)
+    logger.info(info)
+
+GPIO.add_event_detect(CHANNEL, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(CHANNEL, callback)
+
+print("Listening for sound at GPIO %s:%s" % (SHRONA_DEVICE_ID, CHANNEL))
+print("Sending to %s:%s" % (UDP_IP, UDP_PORT))
+while True:
+    sleep(.3)
